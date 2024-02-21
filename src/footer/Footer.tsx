b3e4c6e44826db64b84606b36275ca26d3a7f1eb
Fewatts <feralveswatts@gmail.com>
2024-02-21 16:32:48 -0300

    Primeira parte
    Segunda página ok, sujeita a refatorações

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,10 +1,11 @@
 import github from "./../assets/github.svg"
+import "./Footer.css";
 
 export function Footer() {
     return (
 
         <footer className="footer">
-            <img src={github} alt="githublogo" />
+            <img src={github} alt="githublogo" className="img-footer"/>
         </footer>
     );
 }
--- src/pokemondetail/PokemonDetail.tsx
+++ src/pokemondetail/PokemonDetail.tsx
@@ -45,6 +45,7 @@ export function PokemonDetail() {
                             ))}
                         </ul>
                     </section>
+                    <br /><br /><br /><br /><br /><br /><br /><br />
                 </section>
             )}
         </main>
